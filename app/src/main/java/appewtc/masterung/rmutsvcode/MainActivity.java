4af4aea7b503b6233c3a4218b6d950912f4dad8f
masterUNG <phrombutr@gmail.com>
2017-07-03 15:43:01 +0700

    Design UI NewRegis OK

--- app/src/main/java/appewtc/masterung/rmutsvcode/MainActivity.java
+++ app/src/main/java/appewtc/masterung/rmutsvcode/MainActivity.java
@@ -3,11 +3,25 @@ package appewtc.masterung.rmutsvcode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import appewtc.masterung.rmutsvcode.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.relContent, mainFragment)
+                    .commit();
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/appewtc/masterung/rmutsvcode/fragment/NewRegisterFragment.java
@@ -0,0 +1,34 @@
+package appewtc.masterung.rmutsvcode.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import appewtc.masterung.rmutsvcode.R;
+
+/**
+ * Created by masterUNG on 7/3/2017 AD.
+ */
+
+public class NewRegisterFragment extends Fragment{
+
+    public static NewRegisterFragment newInstance() {
+        NewRegisterFragment newRegisterFragment = new NewRegisterFragment();
+        Bundle bundle = new Bundle();
+        newRegisterFragment.setArguments(bundle);
+        return newRegisterFragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.new_register_fragment_layout, container, false);
+        return view;
+    }
+}   // Main Class
